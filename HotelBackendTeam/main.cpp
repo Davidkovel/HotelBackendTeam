d84e0ee07fee09bbced4aa6c0e681fab43726039
F0sya <nikitachmyria@gmail.com>
2024-10-24 21:04:00 +0300

    Created users classes

--- HotelBackendTeam/main.cpp
+++ HotelBackendTeam/main.cpp
@@ -1,8 +1,11 @@
-#include <iostream>
-using namespace std;
+#include "domain.h"
 
 int main()
 {
+	Guest guest("John", "Doe", "+380230123", 20, 1);
+	Staff staff("John", "Doe", "+380230123", "Manager", 20, 2000);
+	guest.printInfo();
+	staff.printInfo();
 	cout << "[DEBUG] Test run" << endl;
 	return 0;
 }
